2bcf71ee3a8e6c3d6cebf8217e22b9c736342591
Radoslaw Kolodziejczyk <radek940501@interia.pl>
2019-07-23 18:45:59 +0200

    Updates

---
+++ precache-manifest.11f410bd4ff15d451c71cc06b758bda0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "778795ff38cb6f712d0b",
+    "url": "/currency-calculator/static/js/runtime~main.a8f5e66f.js"
+  },
+  {
+    "revision": "d5b2fb4bfbc88939b70d",
+    "url": "/currency-calculator/static/js/main.a19bb24a.chunk.js"
+  },
+  {
+    "revision": "e6f78ffd9df98b25e397",
+    "url": "/currency-calculator/static/js/2.80d25d64.chunk.js"
+  },
+  {
+    "revision": "d5b2fb4bfbc88939b70d",
+    "url": "/currency-calculator/static/css/main.8ca1f61c.chunk.css"
+  },
+  {
+    "revision": "06be39dfc560d33fff2d512aab31d1c5",
+    "url": "/currency-calculator/index.html"
+  }
+];
