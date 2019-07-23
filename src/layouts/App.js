ffd0d7655336c94d65cb02f7305da4c57da6d237
Radoslaw Kolodziejczyk <radek940501@interia.pl>
2019-07-23 18:44:53 +0200

    Update basename in BrowserRouter

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -9,7 +9,7 @@ import Footer from './Footer';
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={ProcessingInstruction.env.PUBLIC_URL}>
         <div className="app">
           <header>
             <Navigation />
